7f16497ba809c7c72583195e33b49181c88dbb2c
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-12-15 01:49:08 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/yildiz/LogIntoActivity.kt
@@ -0,0 +1,11 @@
+package com.geeks.yildiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIntoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_into)
+    }
+}
